694f471d522be4ce6b06064c6fe674599b5e77bf
sefirus <sashasam348@gmail.com>
2022-09-10 12:06:50 +0300

    *check the last commit message

---
+++ WebApi/Mappers/BookMappers/ReadBookMapper.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Core.Interfaces.Mappers;
+using Core.ViewModels.BookViewModels;
+
+namespace WebApi.Mappers.BookMappers;
+
+public class ReadBookMapper : IVmMapper<Book, ReadBookViewModel>
+{
+    public ReadBookViewModel Map(Book source)
+    {
+        var vm = new ReadBookViewModel()
+        {
+            Id = source.Id,
+            Title = source.Title,
+            Description = source.Description,
+            Isbn = source.Isbn,
+            Language = source.Language,
+            PageCount = source.PageCount,
+            //Rate = source.
+        };
+        return vm;
+    }
+}
