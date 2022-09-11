68e3826242048b32a070088ce5635bb8e27a3878
sefirus <sashasam348@gmail.com>
2022-09-11 12:14:47 +0300

    Added mapBoundaries validator

---
+++ WebApi/Validators/MapBoundariesValidator.cs
@@ -0,0 +1,34 @@
+﻿using Core.ViewModels;
+using FluentValidation;
+
+namespace WebApi.Validators;
+
+public class MapBoundariesValidator : AbstractValidator<MapBoundaries>
+{
+    public MapBoundariesValidator()
+    {
+        RuleFor(c => c.South)
+            .GreaterThanOrEqualTo(-90)
+            .WithMessage("Latitude(South) must be greater than -90 deg.")
+            .LessThanOrEqualTo(90)
+            .WithMessage("Latitude(South) must be less than +90 deg.");
+        
+        RuleFor(c => c.North)
+            .GreaterThanOrEqualTo(-90)
+            .WithMessage("Latitude(North) must be greater than -90 deg.")
+            .LessThanOrEqualTo(90)
+            .WithMessage("Latitude(North) must be less than +90 deg.");
+        
+        RuleFor(c => c.East)
+            .GreaterThanOrEqualTo(-180)
+            .WithMessage("Longitude(East) must be greater than -180 deg.")
+            .LessThanOrEqualTo(180)
+            .WithMessage("Longitude(East) must be less than 180 deg.");
+        
+        RuleFor(c => c.West)
+            .GreaterThanOrEqualTo(-180)
+            .WithMessage("Longitude(West) must be greater than -180 deg.")
+            .LessThanOrEqualTo(180)
+            .WithMessage("Longitude(West) must be less than 180 deg.");
+    }
+}
